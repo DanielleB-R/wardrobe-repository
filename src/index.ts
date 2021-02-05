bb03f8914bdcb7b16013560ebbdbb87c0bceb957
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2021-02-05 15:49:15 -0800

    Add logging and json middleware to the Express app

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { APIGatewayProxyEvent, Context } from "aws-lambda";
 import * as serverlessExpress from "aws-serverless-express";
 import express from "express";
+import pinoHttp from "pino-http";
 
 import graphqlHandler from "./graphql";
 
 const createApp = (): express.Express => {
   const app = express();
 
-  // app.get("/", (_, res) => {
-  //   res.send("Welcome!");
-  // });
+  app.use(
+    pinoHttp({ enabled: true }),
+    express.json({ limit: "6MB" }) // Lambda has a payload size limit of 6MB
+  );
 
   app.use("/", graphqlHandler);
 
