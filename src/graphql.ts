94368ab3c8b7ad9560c841d579ed4db7b6129977
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2021-02-05 10:58:37 -0800

    Build the basic GraphQL schema and hook it up

---
+++ src/graphql.ts
@@ -0,0 +1,51 @@
+import * as n from "nexus";
+import { graphqlHTTP } from "express-graphql";
+
+export const Timestamp = n.scalarType({
+  name: "Timestamp",
+  asNexusMethod: "timestamp",
+  description: "A timestamp, encoded in RFC 3336 format",
+});
+
+export const ArticleKind = n.enumType({
+  name: "ArticleKind",
+  members: ["shirt", "pants", "dress", "skirt", "sweater"],
+  description: "A type of clothing article",
+});
+
+export const Article = n.objectType({
+  name: "Article",
+  description: "An article of clothing",
+  definition(t) {
+    t.id("id");
+    t.string("title");
+    t.string("color");
+    t.string("brand");
+    t.field("kind", { type: ArticleKind });
+    t.string("size");
+    t.field("updated", { type: Timestamp });
+  },
+});
+
+export const ArticleQuery = n.queryField("article", {
+  type: Article,
+  description: "A particular article of clothing",
+  args: {
+    id: n.nonNull(n.idArg()),
+  },
+  resolve() {
+    // TODO: Return something vaguely real!
+    return null;
+  },
+});
+
+export const Schema = n.makeSchema({
+  types: [ArticleQuery],
+});
+
+const gqlHandler = graphqlHTTP({
+  schema: Schema,
+  graphiql: true,
+});
+
+export default gqlHandler;
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import { APIGatewayProxyEvent, Context } from "aws-lambda";
 import * as serverlessExpress from "aws-serverless-express";
 import express from "express";
 
+import graphqlHandler from "./graphql";
+
 const createApp = (): express.Express => {
   const app = express();
 
-  app.get("/", (_, res) => {
-    res.send("Welcome!");
-  });
+  // app.get("/", (_, res) => {
+  //   res.send("Welcome!");
+  // });
+
+  app.use("/", graphqlHandler);
+
   return app;
 };
 
@@ -17,4 +22,8 @@ const serverlessApp = serverlessExpress.createServer(app);
 export async function handler(event: APIGatewayProxyEvent, context: Context) {
   return serverlessExpress.proxy(serverlessApp, event, context, "PROMISE")
     .promise;
+}
+
+if (process.env.LOCAL_DEV) {
+  app.listen(8912);
 }
