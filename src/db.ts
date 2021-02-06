4031b10eb8b29e81475f9dedc8492e2b32704a9d
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2021-02-05 16:29:26 -0800

    Remove fake database now that we have a real one

--- src/db.ts
+++ src/db.ts
@@ -16,38 +16,7 @@ const articleSchema = z.object({
 });
 
 export type Article = z.infer<typeof articleSchema>;
-
-interface FakeDB {
-  [key: string]: Article;
-}
-
-const FAKE_DATABASE: FakeDB = {
-  "1": {
-    articleId: "1",
-    title: "Medium weight burgundy cardigan",
-    color: "burgundy",
-    brand: "Picadilly Fashions",
-    kind: "sweater",
-    size: "2X",
-    updated: "2020-12-13T00:00:00Z",
-  },
-  "2": {
-    articleId: "2",
-    title: "Grey heart-pattern t-shirt",
-    color: "grey",
-    brand: "Torrid",
-    kind: "shirt",
-    size: "3X",
-    updated: "2021-01-11T12:00:00Z",
-  },
-};
-
 export async function getArticle(id: string): Promise<Article | null> {
-  const fakeResult = FAKE_DATABASE[id];
-  if (fakeResult) {
-    return fakeResult;
-  }
-
   const queryResult = await docClient
     .get({
       TableName,
